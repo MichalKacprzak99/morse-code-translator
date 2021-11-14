196df83c73dcdba92cf3b91e1f06caf1c130f46e
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-11-14 21:23:36 +0100

    adjust naming | adjust intro label position

--- morse_code_translator/gui/intro_window.py
+++ morse_code_translator/gui/intro_window.py
@@ -11,26 +11,30 @@ class UiIntroWindow(object):
     def setupUi(self, intro_window):
         self.intro_window = intro_window
         self.intro_window.setObjectName("intro_window")
+        self.intro_window.setStyleSheet("background:white")
         self.intro_window.resize(629, 619)
-        self.centralwidget = QtWidgets.QWidget(intro_window)
-        self.centralwidget.setObjectName("centralwidget")
-        self.intro_image_label = QtWidgets.QLabel(self.centralwidget)
-        self.intro_image_label.setGeometry(QtCore.QRect(0, 0, 629, 619))
-        self.intro_image_label.setText("")
-        current_dir = Path(__file__).parent
-        self.intro_image_label.setPixmap(QtGui.QPixmap(str((current_dir / "resources/morse_code_project_logo (1).png"))))
-        self.intro_image_label.setObjectName("intro_image_label")
-        self.info_text_label = CustomLabel(self.centralwidget)
-        self.info_text_label.setGeometry(QtCore.QRect(160, 520, 271, 41))
-        self.info_text_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.info_text_label.setObjectName("info_text_label")
-        self.go_further = QtWidgets.QPushButton(self.centralwidget)
-        self.go_further.setGeometry(QtCore.QRect(0, -5, 629, 619))
-        self.go_further.setText("")
-        self.go_further.setFlat(True)
-        self.go_further.setObjectName("go_further")
+        self.central_widget = QtWidgets.QWidget(intro_window)
+        self.central_widget.setObjectName("central_widget")
 
-        self.intro_window.setCentralWidget(self.centralwidget)
+        self.intro_image = QtWidgets.QLabel(self.central_widget)
+        self.intro_image.setGeometry(QtCore.QRect(0, 0, 629, 619))
+        self.intro_image.setText("")
+        current_dir = Path(__file__).parent
+        self.intro_image.setPixmap(QtGui.QPixmap(str((current_dir / "resources/morse_code_project_logo.png"))))
+        self.intro_image.setObjectName("intro_image")
+
+        self.intro_label = CustomLabel(self.central_widget)
+        self.intro_label.setGeometry(QtCore.QRect(180, 560, 271, 41))
+        self.intro_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.intro_label.setObjectName("intro_label")
+
+        self.go_further_button = QtWidgets.QPushButton(self.central_widget)
+        self.go_further_button.setGeometry(QtCore.QRect(0, -5, 629, 619))
+        self.go_further_button.setText("")
+        self.go_further_button.setFlat(True)
+        self.go_further_button.setObjectName("go_further_button")
+
+        self.intro_window.setCentralWidget(self.central_widget)
 
         self.retranslateUi()
         QtCore.QMetaObject.connectSlotsByName(intro_window)
@@ -38,8 +42,8 @@ class UiIntroWindow(object):
     def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
         self.intro_window.setWindowTitle(_translate("intro_window", "MainWindow"))
-        self.info_text_label.setText(_translate("intro_window", "Click to continue"))
-        self.info_text_label.setFont(QFont('Arial', 1))
+        self.intro_label.setText(_translate("intro_window", "Click to continue"))
+        self.intro_label.setFont(QFont('Arial', 1))
 
 
 class IntroWindow(UiIntroWindow):
@@ -48,14 +52,14 @@ class IntroWindow(UiIntroWindow):
         self.animation = None
         self.setupUi(window)
         self.transform_text()
-        self.go_further.clicked.connect(self.close_window)
+        self.go_further_button.clicked.connect(self.close_window)
 
     def close_window(self):
         self.intro_window.close()
         self.start_window.show()
 
     def transform_text(self):
-        self.animation = QPropertyAnimation(self.info_text_label, b"font")
+        self.animation = QPropertyAnimation(self.intro_label, b"font")
         self.animation.setDuration(8000)
         self.animation.setStartValue(5)
         time_frames = np.linspace(0, 1, num=20)
