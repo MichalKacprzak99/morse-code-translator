25992dd212a398c6bde55139502a05a1b46db7cc
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-11-16 20:24:55 +0100

    make enum unique

--- morse_code_translator/decryption_algorithm/morse_code_symbol.py
+++ morse_code_translator/decryption_algorithm/morse_code_symbol.py
@@ -1,8 +1,8 @@
-from enum import Enum
+from enum import Enum, unique
 
 
 # value dla One, Three, Seven i Default nie jest auto() dla przejrzystych logow
-
+@unique
 class MorseCodeSymbol(Enum):
     One = 'MorseCodeSymbol.One'  # spacja pomiedzy ".", "-"
     Three = 'MorseCodeSymbol.Three'  # spacja pomiedzy literami
