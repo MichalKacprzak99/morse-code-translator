0f902e20a488a169ec37e11ff72a003a113ba606
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-11-17 11:23:02 +0100

    delete redundant code | add abstraction layer for signals | minor refactoring

--- morse_code_translator/gui/utils/arduino_data_collector.py
+++ morse_code_translator/gui/utils/arduino_data_collector.py
@@ -1,32 +1,32 @@
-from typing import Optional
-
 import serial
+
 from PyQt5.QtCore import QObject, QTimer, pyqtSignal
 
 
-class ArduinoDataCollector(QObject):
-    finished = pyqtSignal()
+class ArduinoDataCollectorSignals(QObject):
+    """Defines the signals available from a running ArduinoDataCollector thread."""
     collected_arduino_data = pyqtSignal(bytes)
+
+
+class ArduinoDataCollector(QObject):
 
     def __init__(self, arduino_port: str, arduino_baudrate: int):
         super().__init__()
         self._arduino = serial.Serial()
         self._arduino.baudrate = arduino_baudrate
         self._arduino.port = arduino_port
-
-        self._timer: Optional[QTimer] = None
+        self.signals = ArduinoDataCollectorSignals()
+        self._timer: QTimer = QTimer(self)
+        self._timer.timeout.connect(self.collect_arduino_data)
 
     def start(self):
         # self._arduino.open()
-        self._timer = QTimer(self)
-        self._timer.timeout.connect(self.collect_arduino_data)
         self._timer.start(100)
 
     def stop(self):
         # self._arduino.close()
         self._timer.stop()
-        self.finished.emit()
 
     def collect_arduino_data(self):
         # self.collected_arduino_data.emit(self._arduino.read(1))
-        self.collected_arduino_data.emit(bytes(1))
+        self.signals.collected_arduino_data.emit(bytes(1))
