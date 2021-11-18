bf51d49ffb9cbfb4ecde421d3613c3d60686b0c0
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-11-18 14:50:34 +0100

    change connection initialization with arduino

--- morse_code_translator/gui/utils/arduino_data_collector.py
+++ morse_code_translator/gui/utils/arduino_data_collector.py
@@ -12,21 +12,17 @@ class ArduinoDataCollector(QObject):
 
     def __init__(self, arduino_port: str, arduino_baudrate: int):
         super().__init__()
-        self._arduino = serial.Serial()
-        self._arduino.baudrate = arduino_baudrate
-        self._arduino.port = arduino_port
+        self._arduino = serial.Serial(arduino_port, arduino_baudrate)
         self.signals = ArduinoDataCollectorSignals()
         self._timer: QTimer = QTimer(self)
         self._timer.timeout.connect(self.collect_arduino_data)
 
     def start(self):
-        # self._arduino.open()
         self._timer.start(100)
 
     def stop(self):
-        # self._arduino.close()
         self._timer.stop()
 
     def collect_arduino_data(self):
-        # self.collected_arduino_data.emit(self._arduino.read(1))
-        self.signals.collected_arduino_data.emit(bytes(1))
+        arduino_data = self._arduino.read(1)
+        self.signals.collected_arduino_data.emit(arduino_data)
