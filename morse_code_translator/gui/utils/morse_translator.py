957c295d09e9e2900e55ee886a86aa4a1df05b66
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-11-20 13:18:38 +0100

    add collecting translation statistics

--- morse_code_translator/gui/utils/morse_translator.py
+++ morse_code_translator/gui/utils/morse_translator.py
@@ -1,9 +1,11 @@
-from typing import AnyStr, List
+from pathlib import Path
+from typing import AnyStr, List, Optional
 
 from PyQt5.QtCore import QObject, pyqtSignal
 
 from morse_code_translator.decryption_algorithm.algorithm import convert_from_arduino_to_morse
 from morse_code_translator.decryption_algorithm.morse_code_translation import decrypt_from_morse
+from morse_code_translator.gui.utils.visualize_statistics import visualize_translation_statistics
 
 
 class MorseTranslatorSignals(QObject):
@@ -19,11 +21,14 @@ class MorseTranslator(QObject):
         super().__init__()
         self.signals = MorseTranslatorSignals()
         self._arduino_data: List[AnyStr] = []
+        self.translation_statistics: Optional[dict] = None
 
-    def translate(self, unit_length):
+    def translate(self, time_unit):
         arduino_data = ''.join(self._arduino_data)
-
-        morse_code_from_arduino = convert_from_arduino_to_morse(arduino_data, time_unit=unit_length, interval=0.1)
+        morse_code_from_arduino, translation_statistics = convert_from_arduino_to_morse(arduino_data,
+                                                                                        time_unit=time_unit,
+                                                                                interval=0.1)
+        self.translation_statistics = translation_statistics
         translated_morse_code = decrypt_from_morse(morse_code_from_arduino)
 
         self.signals.morse_code.emit(str(morse_code_from_arduino))
@@ -31,3 +36,7 @@ class MorseTranslator(QObject):
 
     def catch_arduino_data(self, arduino_data: bytes):
         self._arduino_data.append(arduino_data.decode('utf-8'))
+
+    def visualize_translation_statistics(self, save_statistics_to: Path):
+        visualize_translation_statistics(translation_statistics=self.translation_statistics,
+                                         save_statistics_to=save_statistics_to)
